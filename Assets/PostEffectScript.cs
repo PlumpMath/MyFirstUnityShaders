598f96aa6b933d0e7213dbece939a0b14ebf1cd3
Timo Strating <timostrating@home.nl>
2017-03-29 00:12:25 +0200

    first 2 shaders jhee

---
+++ Assets/PostEffectScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostEffectScript : MonoBehaviour {
+
+    public Material mat;
+
+
+	void OnRenderImage( RenderTexture src, RenderTexture dest ) {
+
+        // src is the rendered scene 
+        // dest is out outgoing texture to the GPU
+
+	    Graphics.Blit( src, dest, mat);
+	}
+}
